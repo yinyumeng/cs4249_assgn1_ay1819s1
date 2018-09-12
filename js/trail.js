410a2dbd58221acf2f085120ecd732b1e942de19
yumeng yin <yumeng.yin@hpe.com>
2018-09-12 22:00:45 +0800

    update css

--- js/trail.js
+++
@@ -1,7 +0,0 @@
-$(function () {
-    $('#experiment').click(function() {
-        event.preventDefault();
-        var participantId = window.location.href.split('participantId=')[1];
-        window.location = 'experiment.html?participantId='+participantId;
-    });
-});
